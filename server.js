406aa9d5980846f286735139ced1c820f6a689f8
fabianwifeo <fabian.wifeo@gmail.com>
2022-10-27 10:01:08 +0200

    Loging infos when server is on

--- server.js
+++ server.js
@@ -6,3 +6,11 @@ app.set('port', 4000);
 const server = http.createServer(app);
 
 server.listen(4000);
+
+server.on('listening', () => {
+    console.log('--------------------')
+    console.log('listening on port 4000')
+    console.log('--------------------')
+    console.log('http://localhost:4000/')
+    console.log('--------------------')
+})
